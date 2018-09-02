54a51204d4c453dbe4dfe58323341eefd45da9e3
Jason Franklin <jason.franklin@xing.com>
2018-09-02 12:58:19 +0200

    Make location equatable

--- ToDo/Location.swift
+++ ToDo/Location.swift
@@ -9,12 +9,22 @@
 import Foundation
 import CoreLocation
 
-struct Location {
+struct Location: Equatable {
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        return lhs.name == rhs.name && lhs.coordinates == rhs.coordinates
+    }
+    
     let name: String
     let coordinates: CLLocationCoordinate2D?
     
     init(name: String, coordinates: CLLocationCoordinate2D? = nil) {
         self.name = name
         self.coordinates = coordinates
+    }
+}
+
+extension CLLocationCoordinate2D: Equatable {
+    public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+        return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
     }
 }
--- ToDoTests/LocationTests.swift
+++ ToDoTests/LocationTests.swift
@@ -30,4 +30,36 @@ class LocationTests: XCTestCase {
         XCTAssertEqual(location.coordinates?.longitude, coordinates.longitude)
     }
     
+    func testComparingSameLocations() {
+        let locationName = "This is the location name"
+        let coordinates = CLLocationCoordinate2D(latitude: 1, longitude: 2)
+        
+        // Same name, same coordinates
+        let location1 = Location(name: locationName, coordinates: coordinates)
+        let location2 = Location(name: locationName, coordinates: coordinates)
+        
+        XCTAssertEqual(location1, location2)
+    }
+    
+    func testComparingLocationsDifferentNames() {
+        let locationName1 = "This is the location name"
+        let locationName2 = ""
+        let coordinates = CLLocationCoordinate2D(latitude: 1, longitude: 2)
+        
+        let location1 = Location(name: locationName1, coordinates: coordinates)
+        let location2 = Location(name: locationName2, coordinates: coordinates)
+        
+        XCTAssertNotEqual(location1, location2)
+    }
+    
+    func testComparingLocationsDifferentCoordinates() {
+        let locationName = "This is the name"
+        let coordinates1 = CLLocationCoordinate2D(latitude: 1, longitude: 2)
+        let coordinates2 = CLLocationCoordinate2D(latitude: 0, longitude: 10)
+        
+        let location1 = Location(name: locationName, coordinates: coordinates1)
+        let location2 = Location(name: locationName, coordinates: coordinates2)
+        
+        XCTAssertNotEqual(location1, location2)
+    }
 }
